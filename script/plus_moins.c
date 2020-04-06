1ae0289765854163840a0f5d8d35707877d72301
Andrew270200 <andrewvirginie00@yahoo.com>
2020-04-06 16:09:15 +0400

    v0.2

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -1,31 +1,67 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void plus_moins(int a,int b)
-{
-	if (a > b)
-		printf("plus\n");
-	else if (a < b)
-		printf("moins\n");
-	else
-		printf("Bingo\n");
-}
 
 int main()
 {
 	int a;
 	int b;
-	printf("Entrez votre valeur\n");
+	printf("Veuillez choisir la difficulter du jeux svp : 1(Facile = illimiter) , 2(Moyen = 25) , 3(Difficile = 10)\n");
+	int val;
+	scanf("%d",&val);
+	printf("Entrez votre valeur : ");
 	scanf("%d",&a);
 	b =(rand()%100)+1;
-	printf("Valeur a trouver est : %d \n",b);
+	printf("Valeur a trouver est : %d\n\n ",b);
 
-	while ( b != a )
+	switch(val)
 	{
-		printf("La valeur entrer n'est pas valide veuillez ressayer svp !\n");
-		scanf("%d",&a);
+		case 1:
+			while ( b != a )
+			{
+				printf("La valeur entrer n'est pas valide veuillez ressayer svp !\n");
+				scanf("%d",&a);
+			}
+			printf("Bingo\n");
+			break;
+
+		case 2:
+			for (int x = 0; x <= 25;x++)
+			{
+				if ( b != a )
+				{
+					printf("La valeur entrer n'est pas valide veuillez ressayer svp !\n le nombre d'essaie disponible est de : %d\n", 25 - x);
+					scanf("%d",&a);
+				}	
+				else 
+				{
+					printf("Bingo\n");
+					break;
+				}
+			}
+			break;
+
+		case 3:
+			for (int x = 0; x <= 10;x++)
+			{
+				if ( b != a )
+				{
+					printf("La valeur entrer n'est pas valide veuillez ressayer svp !\n le nombre d'essaie disponible est de : %d\n", 10 - x);
+					scanf("%d",&a);
+				}				
+				else 
+				{
+					printf("Bingo\n");
+					break;
+				}
+			}
+			break;
+
+		default:
+			printf("Vous deviez choisir: 1 , 2 ou 3\n");
+			break;
 	}
-	plus_moins(a,b);
+
 	return 0;
 }
 
